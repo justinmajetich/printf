2c78de1c56fa0622221650a575d0fe062a1da260
justinmajetich <justinmajetich@gmail.com>
2019-10-26 13:55:43 +0000

    Write func to retrieve func pointers

---
+++ fmt_func_caller.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * fmt_func_caller - 
+ *
+ *
+ *
+ *
+ */
+char *(*fmt_func_caller(char *)(char * s)
+{
+	/* struct variable define */
+
+	/* loop to determine fmt specifier */
+		/* return proper function pointer */
+
+	/* return NULL(?) on fail */
+}
