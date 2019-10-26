67bc3d4073f7bf47fd6b7f1822be79a1f13f4b51
justinmajetich <justinmajetich@gmail.com>
2019-10-26 15:31:09 +0000

    Update header file with mngr prototypes

--- holberton.h
+++ holberton.h
@@ -1,6 +1,15 @@
 #ifndef
 #define
 
+#include <stdarg.h>
+
+/* head file */
 int _printf(const char *format, ...);
+
+char* fmt_mngr(char *);
+
+/* funtion callers */
+char *(*fmt_func_caller(char *))(char *s);
+char *(*flag_func_caller(char *))(char *s);
 
 #endif
