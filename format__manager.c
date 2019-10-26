a5159b0249851a708d826daf50bfcfefcc21bff9
KFredlund <katihomes@gmail.com>
2019-10-26 13:29:59 +0000

    Write function outline

---
+++ format__manager.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* fmt_mngr - Function
+*
+*
+* Return:
+*/
+int fmt_mngr()
+{
+	//interpret format specifier
+		//if true, select correct function
+		//else print error message
+	//if not string, convert to string
+	//interpret flags
+		//if true, select correct function
+	//format string according to flag
+	// return to printf
+	return ();
+}
