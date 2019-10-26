8ba64e188c84a169599c6e43269d6c04106f1432
KFredlund <katihomes@gmail.com>
2019-10-26 14:39:04 +0000

    Write a function that encodes a string to Rot13

---
+++ R_rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* R_rot13 - function that encodes a string using rot13
+* @s: string to be encoded
+* Return: Rot13'd string
+*/
+char *R_rot13(char *s)
+{
+	int i, j;
+	char *in = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (s[i] == in[j])
+			{
+				s[i] = out[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
