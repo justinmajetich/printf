e992a15f2d3b02502b53ce3eee181721905b0ee8
KFredlund <katihomes@gmail.com>
2019-10-26 14:09:04 +0000

    Write an outline for the flag selector function pointer

---
+++ flag_func_caller.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*
+* flag_func_caller - Function that selects the correct function based on flag
+*
+*
+* Return:
+*/
+char *(*flag_func_caller(char *))(char *)
+{
+	//define our struct to reference against
+	//loop through and compare flag
+		//select flag if true
+		//else, return maybe null
+
+	return (); //maybe NULL
+}
