cc4d7ccfbfbe1d565324e03e9693d9ff9e8c06c6
justinmajetich <justinmajetich@gmail.com>
2019-10-27 15:53:22 +0000

    Update header file with structs and protos

--- holberton.h
+++ holberton.h
@@ -1,15 +1,55 @@
-#ifndef
-#define
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdarg.h>
 
-/* head file */
+/* STRUCTURES */
+/**
+ * li_fps - functions to convert long int args
+ * @spec: conversion specifier
+ * @f: function pointer
+ */
+struct li_fps
+{
+	char *spec;
+	char *(*f)(long int);
+};
+
+/**
+ * lu_fps - functions to convert long unsigned args
+ * @spec: conversion specifier
+ * @f: function pointer
+ */
+struct lu_fps
+{
+	char *spec;
+	char *(*f)(long int);
+};
+
+/**
+ * ptr_fps - functions to convert pointer args
+ * @spec: conversion specifier
+ * @f: function pointer
+ */
+struct ptr_fps
+{
+	char *spec;
+	char *(*f)(long int);
+};
+
+/* PARENT FUNCTION */
 int _printf(const char *format, ...);
 
-char* fmt_mngr(char *);
+/* MANAGER FUNCTIONS */
+char *fmt_mngr(char *, char *);
+char *(*li_func_mngr(char *))(char *, long int);
+char *(*lu_func_mngr(char *))(char *, unsigned long);
+char *(*ptr_func_mngr(char *))(char *, char *);
 
-/* funtion callers */
-char *(*fmt_func_caller(char *))(char *s);
-char *(*flag_func_caller(char *))(char *s);
+/* CONVERSION FUNCTIONS */
+
+/* FORMATTING FUNCTIONS */
+
+/* HELPER FUNCTIONS */
 
 #endif
