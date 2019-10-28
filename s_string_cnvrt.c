ac730f0b201af2f4d08f614e4bd2b7fe16b8967f
KFredlund <katihomes@gmail.com>
2019-10-28 21:29:35 +0000

    Create a function that returns a given string input

---
+++ s_string_cnvrt.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#define NULL 0
+/**
+* s_string_cnvrt - A function that returns a string
+* @s: returns a string
+* Return: The string input
+*/
+char *s_string_cnvrt(char *s)
+{
+	int j;
+	char *print_s;
+
+	print_s = malloc(sizeof(char) * (_strlen(s) + 1));
+	if (print_s == NULL)
+		return (NULL);
+	for (j = 0; s[j] != '\0'; j++)
+		print_s[j] = s[j];
+	print_s[j] = '\0';
+	return (print_s);
+}
