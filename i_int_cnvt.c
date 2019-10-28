b453025f71b4137d3987d1aabe69587a718bb0cd
KFredlund <katihomes@gmail.com>
2019-10-28 22:15:12 +0000

    A function that converts an int to a string

---
+++ i_int_cnvt.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#define NULL 0
+/**
+* i_int_cnvrt - Function that converts an integer
+* input to a string
+* @num: int var to be converted to string
+* Return: Converted string
+*
+*/
+char *i_int_cnvrt(long int num)
+{
+	char *num_to_s;
+
+	num_to_s = malloc(sizeof(char) * 100);
+	if (num_to_s == NULL)
+		return (NULL);
+	return (num_to_s = _itoa(num, num_to_s, 10));
+}
