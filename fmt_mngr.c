a3983a538f0c7d6e4a3aa16b6ab3b7eaa2bd5c50
KFredlund <katihomes@gmail.com>
2019-10-29 15:57:40 +0000

    Correct comment syntax and remove trailing whitespace

--- fmt_mngr.c
+++ fmt_mngr.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
-*
-* fmt_mngr - directs/retrieves data to/from formatting and conversion funcs
+* fmt_mngr - directs/retrieves data to/from formatting and
+* conversion funcs
 * @args: arg list
-* @index: pointer to start of format spec in format string
-* 
+* @fmt_spec: pointer to start of format spec in format string
+*
 * Return: converted string on Success, NULL on Fail
 */
 char *fmt_mngr(va_list args, char *fmt_spec)
 {
 	int arg_type = 0;
 	char *cnvrtd_str; /* post-conversion arg string */
-	
+
 	char *(*cnvrt_li)(long int);
 	char *(*cnvrt_lu)(unsigned long);
 	char *(*cnvrt_ptr)(char *);
@@ -32,7 +32,7 @@ char *fmt_mngr(va_list args, char *fmt_spec)
 				break;
 			case 2:
 				cnvrt_lu = lu_fp_mngr(fmt_spec);
-				cnvrtd_str = cnvrt_lu(va_arg(args, unsigned));
+				cnvrtd_str = cnvrt_lu(va_arg(args, unsigned int));
 				break;
 			case 3:
 				cnvrt_lu = lu_fp_mngr(fmt_spec);
@@ -43,8 +43,7 @@ char *fmt_mngr(va_list args, char *fmt_spec)
 				cnvrtd_str = cnvrt_ptr(va_arg(args, char *));
 				break;
 		}
-	/* format converted arg according to flags 
-	fmttd_str = fmt_mngr(fmt_spec, cnvrtd_str);
-	*/
-	return (cnvrtd_str);;
+/* format converted arg according to flags */
+/* fmttd_str = fmt_mngr(fmt_spec, cnvrtd_str); */
+	return (cnvrtd_str);
 }
